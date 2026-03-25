a27254be004790cacb870b2d569038140ec82ed1
Syed Muneeb <muneebmurshid786@gmail.com>
2026-03-25 12:35:50 +0000

    feat: add controllers and DbSeeder

---
+++ FoodInspectionTracker.MVC/Controllers/FollowUpController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FoodInspectionTracker.Domain;
+using FoodInspectionTracker.MVC.Data;
+
+namespace FoodInspectionTracker.MVC.Controllers
+{
+    public class FollowUpController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public FollowUpController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FollowUp
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.FollowUps.Include(f => f.Inspection);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: FollowUp/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps
+                .Include(f => f.Inspection)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+
+            return View(followUp);
+        }
+
+        // GET: FollowUp/Create
+        public IActionResult Create()
+        {
+            ViewData["InspectionId"] = new SelectList(_context.Inspections, "Id", "Id");
+            return View();
+        }
+
+        // POST: FollowUp/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,InspectionId,DueDate,Status,ClosedDate,Notes")] FollowUp followUp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(followUp);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InspectionId"] = new SelectList(_context.Inspections, "Id", "Id", followUp.InspectionId);
+            return View(followUp);
+        }
+
+        // GET: FollowUp/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps.FindAsync(id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+            ViewData["InspectionId"] = new SelectList(_context.Inspections, "Id", "Id", followUp.InspectionId);
+            return View(followUp);
+        }
+
+        // POST: FollowUp/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,InspectionId,DueDate,Status,ClosedDate,Notes")] FollowUp followUp)
+        {
+            if (id != followUp.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(followUp);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FollowUpExists(followUp.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InspectionId"] = new SelectList(_context.Inspections, "Id", "Id", followUp.InspectionId);
+            return View(followUp);
+        }
+
+        // GET: FollowUp/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var followUp = await _context.FollowUps
+                .Include(f => f.Inspection)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (followUp == null)
+            {
+                return NotFound();
+            }
+
+            return View(followUp);
+        }
+
+        // POST: FollowUp/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var followUp = await _context.FollowUps.FindAsync(id);
+            if (followUp != null)
+            {
+                _context.FollowUps.Remove(followUp);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FollowUpExists(int id)
+        {
+            return _context.FollowUps.Any(e => e.Id == id);
+        }
+    }
+}
--- FoodInspectionTracker.MVC/Data/AppDbContext.cs
+++ FoodInspectionTracker.MVC/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using FoodInspectionTracker.Domain;
-using System;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
